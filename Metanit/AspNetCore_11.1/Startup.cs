19819dd3e4cdbff74954887d8c16697a6ec5f9e8
Andrew <a13andrew@gmail.com>
2018-12-01 15:42:12 +0200

    Routing with constrains

--- Metanit/AspNetCore_11.1/Startup.cs
+++ Metanit/AspNetCore_11.1/Startup.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AspNetCore_11._1
@@ -15,6 +17,7 @@ namespace AspNetCore_11._1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,45 @@ namespace AspNetCore_11._1
                 app.UseDeveloperExceptionPage();
             }
 
+            //Display all components and their values
+            RouteHandler handler= new RouteHandler(async context => {
+                await context.Response.WriteAsync(Stringify(context.GetRouteData()
+                    .Values
+                    .Select(x => KeyValuePair.Create<string, string>(x.Key, (string)x.Value))));
+            });
+
+            //Map handler to URI like .../Id/A13Andrew
+            app.UseRouter(new RouteBuilder(app, handler).MapRoute("getall", "Id/{username}/{Area=Profile}/{Controller?}/{Actionn?}/{*all}").Build());
+
+
+            //Display controller and action components values 
+            handler = new RouteHandler(async (context)=> {
+                
+                context.Response.ContentType = "text/html charset=UTF-8";
+                string Controller = context.GetRouteData().Values["Controller"].ToString();
+                string Action = context.GetRouteValue("Action").ToString();
+                string other = context.Request.Query.Select(x => x.Key +"="+ x.Value.ToString()).Aggregate((x1,x2)=>x1+="</br>"+x2);
+                await context.Response.WriteAsync($"<p>You've entered custom path where:</p> </br>Controller={Controller}</br>Action={Action}</br>Parametrs:{other}");
+            });
+
+            IRouteBuilder builder = new RouteBuilder(app,handler);
+            builder.MapRoute("default", "{Controller}/{Action}");
+            app.UseRouter(builder.Build());
+
+
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("No url match");
             });
+        }
+        public static string Stringify(IEnumerable<KeyValuePair<string,string>> array)
+        {
+            string output = "";
+            foreach (KeyValuePair<string,string> item in array)
+                output += item.Key+"="+item.Value+Environment.NewLine;
+
+            return output;
         }
     }
 }
---
+++ Metanit/AspnetCore_11.2/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspnetCore_11._2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            #region Main
+            //store routes
+            var AvaliableRoutes = new List<Tuple<string, string>>();
+
+            var builder = new RouteBuilder(app);
+
+            foreach (var item in new[] { "bool","int", "alpha","minlength(3)" ,"regex(^H.*)"})
+            {
+                // setting route
+                // suppose item="int"
+                // then route="int/{value:int}"
+                // means that URL like https://localhost:xxxx/int/4343 - will work
+                // where value=4343, now you can vary value and watch how 'item' constraint 
+                // affects URL matching
+
+                string route = item + "/{value:" + item + "}";
+                // store avaliable routes somewhere to display them URL mathing failed
+                AvaliableRoutes.Add(new Tuple<string, string>(item, route));
+                // add routes with their handlers to RouteBuilder
+                builder.MapGet(route, async (context) =>
+                {
+                    //get velue from RouteData
+                    string value = context.GetRouteValue("value").ToString();
+                    //get request path
+                    string path = context.Request.Path.ToString();
+                    //make response message
+                    string message = $"Request Path={path}"+Environment.NewLine+$"value={value}";
+                    //set content type to plain text istead of html, why? who knows
+                    context.Response.ContentType = "text/plain";
+                    //write message to output stream
+                    await context.Response.WriteAsync(message);
+                });
+
+            }
+
+            app.UseRouter(builder.Build());
+            
+
+            app.Run(async (context) =>
+            {
+                //set left part of URI
+                string host = (context.Request.IsHttps ? "https://" : "http://") + context.Request.Host.Value;
+
+                string output = "<ul>";
+
+                //add href tag
+                foreach (var item in AvaliableRoutes)
+                    output += $"<li><a href={host}/{item.Item1}/><b>{item.Item2}</b></a></li>";
+                output += "</ul>";
+                context.Response.ContentType = "text/html charset=UTF-8";
+                await context.Response.WriteAsync(output);
+
+            });
+            #endregion
+        }
+    }
+}
