41a1e5f7260d3d845302af6cea66f1244c9a7455
Andrew <a13andrew@gmail.com>
2018-12-14 21:25:28 +0200

    still learing

---
+++ Metanit/AspNetCore_19.1/Main.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_19._1
+{
+    public class Account
+    {
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public Account LoopIn { get; set; }
+
+    }
+
+    public class AccountController : Controller
+    {
+        public IActionResult getjson()
+        {
+
+            //Account a = new Account()
+            //{
+            //    Name = "Luke",
+            //    Password = "1234",
+            //    Email = "very@mail.kz"
+            //};
+
+            //Account b = new Account()
+            //{
+            //    Name = "Darte",
+            //    Password = "1234",
+            //    Email = "very2@mail.kz",
+            //    LoopIn = a
+            //};
+            //a.LoopIn = b;
+            var result = JsonConvert.SerializeObject(this, new JsonSerializerSettings
+            {
+                MaxDepth=2,
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects
+
+
+            });
+
+            return Content(result);
+
+            //return Json(this, new Newtonsoft.Json.JsonSerializerSettings
+            //{
+            //    Formatting = Newtonsoft.Json.Formatting.Indented
+            //});
+
+            //return Json(this.ControllerContext, new Newtonsoft.Json.JsonSerializerSettings
+            //{
+            //    Formatting = Newtonsoft.Json.Formatting.Indented
+            //});
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Account acc)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content($"{acc.Name}-{acc.Password}-{acc.Email}");
+            }
+            else
+                return View(acc);
+        }
+    }
+}
---
+++ Metanit/AspNetCore_8.3/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCore_8._3.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCore_8._3.Controllers
+{
+    public class A
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public B B { get; set; }
+        public IEnumerable<int> col { get; set; }
+    }
+
+    public class B : A
+    {
+
+    }
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult GetA(A a)
+        {
+            return View();
+        }
+
+        public IActionResult GetAPost(A a)
+        {
+            var settings = new Newtonsoft.Json.JsonSerializerSettings
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+
+            };
+
+            var result = "QUERY:" + Environment.NewLine + JsonConvert.SerializeObject(Request.Query, settings) + Environment.NewLine;
+
+            if (Request.HasFormContentType)
+            {
+                result += "FORM" + Environment.NewLine + JsonConvert.SerializeObject(Request.Form, settings) + Environment.NewLine;
+            }
+
+            result += "ROUTE" + Environment.NewLine + JsonConvert.SerializeObject(RouteData, settings) + Environment.NewLine;
+            result += "OBJECT" + Environment.NewLine + JsonConvert.SerializeObject(a, settings) + Environment.NewLine;
+
+
+
+            return Content(result);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
