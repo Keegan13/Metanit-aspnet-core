7be122570e766813ee599f44c6c2b3fbe0ef4c62
Andrew <a13andrew@gmail.com>
2018-12-26 21:59:54 +0200

    save

---
+++ Metanit/CodeTagHelper/MAIN/CodeTagHelper.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace CustomTagHelper.TagHelpers
+{
+    [HtmlTargetElement("pre")]
+    [HtmlTargetElement("hljs")]
+    public class HighLightJSTaghelper : TagHelper
+    {
+        
+        [HtmlAttributeName("hljs-lang")]
+        public string Language { get; set; }
+        [HtmlAttributeName("style")]
+        public string Style { get; set; }
+        [HtmlAttributeName("script")]
+        public string Script { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+           
+            TagBuilder code = new TagBuilder("code");
+            code.AddCssClass($"{Language}");
+            
+            var content = (output.GetChildContentAsync().Result).GetContent();
+
+            code.InnerHtml.AppendHtml(content);
+            
+            StringWriter ms = new StringWriter();
+            code.WriteTo(ms,HtmlEncoder.Default);
+
+            output.Content.SetHtmlContent(ms.ToString());
+        }
+        //private string Escape(string input)
+        //{
+        //    return OutputElementHintAttribute;
+        //}
+        
+
+    }
+    
+}
---
+++ Metanit/CodeTagHelper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeTagHelper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
